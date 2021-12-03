2ae9b995849bd68668376435ec2c5b551d9f3d9c
Ayush Soni <mr.soni1002001@gmail.com>
2021-12-03 20:29:27 +0530

    🚀fix: newsletter subscribe button outline (#13)

--- components/Home/Newsletter.js
+++ components/Home/Newsletter.js
@@ -11,19 +11,21 @@ const Newsletter = () => {
           </h1>
 
           <div className="flex flex-col md:flex-row">
-          <input
-            className="appearance-none mt-4 md:mt-0 border rounded-md w-full py-3 mr-6  px-3 text-gray-700 leading-tight focus:outline-none  focus:border-my-orange"
-            id="username"
-            type="text"
-            placeholder="Enter Your Name"
-          />
-          <input
-            className="appearance-none mt-4 md:mt-0 border rounded-md w-full py-3 mr-6  px-3 text-gray-700 leading-tight focus:outline-none  focus:border-my-orange"
-            id="username"
-            type="text"
-            placeholder="Enter Your Email"
-          />
-          <button className="bg-my-orange mt-4 md:mt-0 shadow text-white px-8 py-3 rounded-md hover:bg-opacity-75">Subscribe</button>
+            <input
+              className="appearance-none mt-4 md:mt-0 border rounded-md w-full py-3 mr-6  px-3 text-gray-700 leading-tight focus:outline-none  focus:border-my-orange"
+              id="username"
+              type="text"
+              placeholder="Enter Your Name"
+            />
+            <input
+              className="appearance-none mt-4 md:mt-0 border rounded-md w-full py-3 mr-6  px-3 text-gray-700 leading-tight focus:outline-none  focus:border-my-orange"
+              id="username"
+              type="text"
+              placeholder="Enter Your Email"
+            />
+            <button className="bg-my-orange mt-4 md:mt-0 shadow text-white px-8 py-3 rounded-md hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-my-orange">
+              Subscribe
+            </button>
           </div>
         </div>
       </div>
