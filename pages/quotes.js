8b440f36c333321b0c625d2ec34d4fb41d04e5dd
Amritpal Singh <matharooamrit098@gmail.com>
2021-12-04 16:18:00 +0530

    testing netilfy

--- pages/quotes.js
+++ pages/quotes.js
@@ -3,7 +3,7 @@ import React from 'react'
 const quotes = () => {
     return (
         <div>
-            <h1>Testing netlify</h1>
+            <h1>Testing netlify again</h1>
         </div>
     )
 }
