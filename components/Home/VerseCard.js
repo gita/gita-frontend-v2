a0773984f08f3d9a4063db4babda2f89ddaf07b2
Akshit Arora <akshitarora9211@gmail.com>
2021-12-08 13:09:21 +0530

    add graphql and apollo and data fetch

--- components/Home/VerseCard.js
+++ components/Home/VerseCard.js
@@ -1,30 +1,30 @@
 import React from "react";
 import Link from "next/link";
 
-const VerseCard = () => {
+const VerseCard = ({ chapter }) => {
   return (
-    <Link href="/verse">
-      <div className="bg-white shadow-xl  border-2 border-white mt-6 rounded-md p-6 hover:bg-box-bg hover:shadow-none hover:border-box-stroke hover:border-2 hover:cursor-pointer">
-        <h3 className="text-my-orange font-bold">Chapter 1</h3>
-        <h2 className="text-xl font-bold">Sraddhatraya Vibhaga Yoga</h2>
-        <p className="text-gray-500 mt-2">
-          Cupiditate maiores ullam eveniet adipisci in doloribus nulla minus.
-          Voluptas iusto libero adipisci rem et corporis.
-        </p>
+    // <Link href={`/chapter/${chapter.id}`}>
+    <Link href='/verse'>
+      <div className='flex flex-col bg-white shadow-xl  border-2 border-white mt-6 rounded-md p-6 hover:bg-box-bg hover:shadow-none hover:border-box-stroke hover:border-2 hover:cursor-pointer'>
+        <h3 className='text-my-orange font-bold'>
+          Chapter {chapter.chapterNumber}
+        </h3>
+        <h2 className='text-xl font-bold'>{chapter.nameTranslated}</h2>
+        <p className='flex-1 text-gray-500 mt-2'>{chapter.chapterSummary}</p>
 
-        <div className="flex justify-between">
-          <div className="flex text-sm items-center mt-4">
-            <img src="/list.svg" className="h-5 w-5 mr-4" />
-            42 Verses
+        <div className='flex justify-between'>
+          <div className='flex text-sm items-center mt-4'>
+            <img src='/list.svg' className='h-5 w-5 mr-4' />
+            {chapter.versesCount} Verses
           </div>
 
-          <div className="flex mt-4">
-            <div className="flex text-sm items-center align-middle mr-3">
-              <img src="/bookmark.svg" className="h-4  w-4 mr-1" />2
+          <div className='flex mt-4'>
+            <div className='flex text-sm items-center align-middle mr-3'>
+              <img src='/bookmark.svg' className='h-4  w-4 mr-1' />2
             </div>
 
-            <div className="flex text-sm items-center">
-              <img src="/shuffle.svg" className="h-5 w-5 mr-1" />2
+            <div className='flex text-sm items-center'>
+              <img src='/shuffle.svg' className='h-5 w-5 mr-1' />2
             </div>
           </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,62 @@
-import {useEffect} from 'react'
-import Head from 'next/head'
-import Banner from '../components/Home/Banner'
-import Chapters from '../components/Home/Chapters'
-import Newsletter from '../components/Home/Newsletter'
-import VerseOfDay from '../components/Home/VerseOfDay'
-import HomeLayout from '../layouts/HomeLayout'
+import { useEffect } from "react";
+import Head from "next/head";
+import Banner from "../components/Home/Banner";
+import Chapters from "../components/Home/Chapters";
+import Newsletter from "../components/Home/Newsletter";
+import VerseOfDay from "../components/Home/VerseOfDay";
+import HomeLayout from "../layouts/HomeLayout";
 import { useDispatch, useSelector } from "react-redux";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
+export const getStaticProps = async () => {
+  const client = new ApolloClient({
+    uri: "https://gql.bhagavadgita.io/graphql",
+    cache: new InMemoryCache(),
+  });
 
-export default function Home() {
-
-  const dispatch = useDispatch()
-  const settings = useSelector(state => state.settings);
+  const { data } = await client.query({
+    query: gql`
+      query MyQuery {
+        allGitaChapters {
+          nodes {
+            id
+            chapterNumber
+            chapterSummary
+            nameTranslated
+            versesCount
+          }
+        }
+      }
+    `,
+  });
+  return {
+    props: { chapters: data.allGitaChapters.nodes },
+  };
+};
+export default function Home({ chapters }) {
+  const dispatch = useDispatch();
+  const settings = useSelector((state) => state.settings);
 
   useEffect(() => {
-    console.log(settings)
-  })
+    console.log(settings);
+  });
 
   return (
-    <div className="font-inter min-h-screen py-2">
+    <div className='font-inter min-h-screen py-2'>
       <Head>
         <title>Bhagwat Gita App</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link ref="style" rel="stylesheet" href="/globals.css"/>
+        <link rel='icon' href='/favicon.ico' />
+        <link ref='style' rel='stylesheet' href='/globals.css' />
       </Head>
 
-      <main className="">
-      <HomeLayout>
-       <Banner/>
-       <VerseOfDay/>
-       <Newsletter/>
-       <Chapters/>
-       </HomeLayout>
+      <main className=''>
+        <HomeLayout>
+          <Banner />
+          <VerseOfDay />
+          <Newsletter />
+          <Chapters chapters={chapters} />
+        </HomeLayout>
       </main>
-
     </div>
-  )
+  );
 }
