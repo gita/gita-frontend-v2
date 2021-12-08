d4ec673355ae153655aa9b4c2235512c8f0bc5e7
akshatj2209 <akshatj2209@gmail.com>
2021-12-08 15:00:52 +0530

    feat: Add Audio in Verse page

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,36 @@
-import {useEffect} from 'react'
-import Head from 'next/head'
-import Banner from '../components/Home/Banner'
-import Chapters from '../components/Home/Chapters'
-import Newsletter from '../components/Home/Newsletter'
-import VerseOfDay from '../components/Home/VerseOfDay'
-import HomeLayout from '../layouts/HomeLayout'
+import { useEffect } from "react";
+import Head from "next/head";
+import Banner from "../components/Home/Banner";
+import Chapters from "../components/Home/Chapters";
+import Newsletter from "../components/Home/Newsletter";
+import VerseOfDay from "../components/Home/VerseOfDay";
+import HomeLayout from "../layouts/HomeLayout";
 import { useDispatch, useSelector } from "react-redux";
 
-
 export default function Home() {
-
-  const dispatch = useDispatch()
-  const settings = useSelector(state => state.settings);
+  const dispatch = useDispatch();
+  const settings = useSelector((state) => state.settings);
 
   useEffect(() => {
-    console.log(settings)
-  })
+    console.log(settings);
+  });
 
   return (
-    <div className="font-inter min-h-screen py-2">
+    <div className='font-inter min-h-screen py-2'>
       <Head>
         <title>Bhagwat Gita App</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link ref="style" rel="stylesheet" href="/globals.css"/>
+        <link rel='icon' href='/bhagavad-gita.png' />
+        <link ref='style' rel='stylesheet' href='/globals.css' />
       </Head>
 
-      <main className="">
-      <HomeLayout>
-       <Banner/>
-       <VerseOfDay/>
-       <Newsletter/>
-       <Chapters/>
-       </HomeLayout>
+      <main className=''>
+        <HomeLayout>
+          <Banner />
+          <VerseOfDay />
+          <Newsletter />
+          <Chapters />
+        </HomeLayout>
       </main>
-
     </div>
-  )
+  );
 }
