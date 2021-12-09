98dfc4aff2bfe5c95661b6c3241b4a36016f5a8c
Akshit Arora <akshitarora9211@gmail.com>
2021-12-10 01:45:30 +0530

    add links for chapter navigations

--- pages/chapter/[chapterNumber].js
+++ pages/chapter/[chapterNumber].js
@@ -1,6 +1,7 @@
 import React from "react";
 import PagesLayout from "../../layouts/PagesLayout";
 import Head from "next/head";
+import Link from "next/link";
 import {
   SortAscendingIcon,
   UsersIcon,
@@ -80,65 +81,74 @@ export default function Chapter({ chapterData }) {
     gitaVersesByChapterId,
   } = chapterData;
   const verses = gitaVersesByChapterId.nodes;
+  const nextChapter = chapterNumber + 1;
+  const previousChapter = chapterNumber - 1;
   return (
     <div>
       <Head>
         <title>Bhagwat Gita App - Chapters</title>
-        <link rel='icon' href='/favicon.ico' />
-        <link ref='style' rel='stylesheet' href='/globals.css' />
+        <link rel="icon" href="/favicon.ico" />
+        <link ref="style" rel="stylesheet" href="/globals.css" />
       </Head>
 
-      <div className='max-w-5xl font-inter py-24 chapter-intro mx-auto text-center  px-4 sm:px-6'>
-        <img
-          src='/arrow-left.png'
-          className='fixed z-neg top-1/2 md:top-1/3 left-3'
-        />
-        <img
-          src='/arrow-right.png'
-          className='fixed z-neg top-1/2 md:top-1/3 right-3'
-        />
-
-        <h3 className='text-my-orange font-medium uppercase'>
+      <div className="max-w-5xl font-inter py-24 chapter-intro mx-auto text-center  px-4 sm:px-6">
+        {previousChapter >= 1 && (
+          <Link href={`/chapter/${previousChapter}`}>
+            <img
+              src="/arrow-left.png"
+              className="fixed z-neg top-1/2 md:top-1/3 left-3 hover:brightness-90 hover:cursor-pointer"
+            />
+          </Link>
+        )}
+        {nextChapter <= 18 && (
+          <Link href={`/chapter/${nextChapter}`}>
+            <img
+              src="/arrow-right.png"
+              className="fixed z-neg top-1/2 md:top-1/3 right-3 hover:brightness-90 hover:cursor-pointer"
+            />
+          </Link>
+        )}
+        <h3 className="text-my-orange font-medium uppercase">
           Chapter {chapterNumber}
         </h3>
-        <h1 className='font-extrabold text-3xl my-8'>{nameTranslated}</h1>
-        <p className='text-left  mt-3'>{chapterSummary}</p>
+        <h1 className="font-extrabold text-3xl my-8">{nameTranslated}</h1>
+        <p className="text-left  mt-3">{chapterSummary}</p>
       </div>
 
-      <div className='max-w-5xl font-inter mx-auto text-center  px-4 sm:px-6'>
-        <div className='flex items-center justify-between border-t py-6 border-b border-gray-200'>
-          <div className='font-extrabold'>{versesCount} Verses</div>
-          <div className='mt-1 flex rounded-md shadow-sm'>
-            <div className='relative flex items-stretch flex-grow focus-within:z-10'>
-              <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'></div>
+      <div className="max-w-5xl font-inter mx-auto text-center  px-4 sm:px-6">
+        <div className="flex items-center justify-between border-t py-6 border-b border-gray-200">
+          <div className="font-extrabold">{versesCount} Verses</div>
+          <div className="mt-1 flex rounded-md shadow-sm">
+            <div className="relative flex items-stretch flex-grow focus-within:z-10">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"></div>
               <input
-                type='text'
-                name='email'
-                id='email'
-                className='focus:ring-my-orange border focus:border-my-orange block w-full rounded-none rounded-l-md pl-2 sm:text-sm border-gray-300'
-                placeholder='Go To Verse'
+                type="text"
+                name="email"
+                id="email"
+                className="focus:ring-my-orange border focus:border-my-orange block w-full rounded-none rounded-l-md pl-2 sm:text-sm border-gray-300"
+                placeholder="Go To Verse"
               />
             </div>
             <button
-              type='button'
-              className='-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-my-orange focus:border-my-orange'
+              type="button"
+              className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-my-orange focus:border-my-orange"
             >
               <SortAscendingIcon
-                className='h-5 w-5 text-gray-400'
-                aria-hidden='true'
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
               />
               <span>Sort</span>
 
               <ChevronDownIcon
-                className='h-5 w-5 text-gray-400'
-                aria-hidden='true'
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
               />
             </button>
           </div>
         </div>
       </div>
 
-      <div className='max-w-5xl font-inter py-8 mb-16 mx-auto   px-4 sm:px-6'>
+      <div className="max-w-5xl font-inter py-8 mb-16 mx-auto   px-4 sm:px-6">
         {verses.map((verse) => (
           <VerseList verseData={verse} key={verse.id} />
         ))}
