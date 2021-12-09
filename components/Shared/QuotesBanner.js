45cd07771888aca0b8ec79972158410cb6e2e323
Akshit Arora <akshitarora9211@gmail.com>
2021-12-10 03:05:10 +0530

    changes: - add quotes data from figma - uncomment quotes page code

--- components/Shared/QuotesBanner.js
+++ components/Shared/QuotesBanner.js
@@ -1,17 +1,13 @@
-import React from 'react'
-import HomeLayout from "../layouts/HomeLayout";
+import React from "react";
 
 export default function QuotesBanner() {
-    return (
-        <>
-          <div className="max-w-full  mx-auto  xl:px-24 z-50">
-            <div className="about xl:rounded-lg flex flex-col h-4/5 px-8 py-36">
-              <h1 className="text-3xl md:text-5xl text-center font-extrabold text-shadow text-white t-shadow">
-              Bhagavad Gita Quotes By Lord Krishna
-              </h1>
-            </div>
-          </div>
-        </>
-      );
-    }
-    
+  return (
+    <div className="max-w-full mx-auto xl:px-24 z-50">
+      <div className="quote xl:rounded-lg flex flex-col h-4/5 px-8 py-36">
+        <h1 className="text-3xl md:text-5xl text-center font-extrabold text-shadow text-white t-shadow">
+          Bhagavad Gita Quotes By <br /> Lord Krishna
+        </h1>
+      </div>
+    </div>
+  );
+}
