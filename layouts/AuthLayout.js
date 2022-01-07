46bab7994ad9a657f66688d30f28154fad55e9a0
Akshit <akshitarora9211@gmail.com>
2022-01-07 14:28:21 +0530

    add AuthLayout file

---
+++ layouts/AuthLayout.js
@@ -0,0 +1,8 @@
+import React from "react";
+const AuthLayout = ({ children }) => {
+  return (
+    <div className="flex justify-center items-center w-full">{children}</div>
+  );
+};
+
+export default AuthLayout;
