a161fcbf83e823185acbcc5d54db8962dfd099f3
Akshit <akshitarora9211@gmail.com>
2022-01-07 23:36:41 +0530

    fix auth layout height

--- layouts/AuthLayout.js
+++ layouts/AuthLayout.js
@@ -3,7 +3,7 @@ import AuthHeader from "../components/Headers/AuthHeader";
 
 const AuthLayout = ({ children }) => {
   return (
-    <div className="h-screen w-full bg-auth-bg">
+    <div className="min-h-screen w-full bg-auth-bg relative">
       <div className="auth-bg" />
       <AuthHeader />
       <div className="flex justify-center items-center w-full">{children}</div>
