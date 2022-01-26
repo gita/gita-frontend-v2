cc69e60c732cd06d516d6c8f3d5ee448cbe03bcf
Akshit <akshitarora9211@gmail.com>
2022-01-26 13:00:35 +0530

    remove apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -1,49 +1,13 @@
 import "tailwindcss/tailwind.css";
 import { wrapper } from "../redux/store";
 import { ThemeProvider } from "next-themes";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-  gql,
-} from "@apollo/client";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
-  const client = new ApolloClient({
-    link: new createHttpLink({
-      uri: "https://gql.bhagavadgita.io/graphiql",
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-    }),
-    cache: new InMemoryCache(),
-  });
-  client
-    .query({
-      query: gql`
-        query MyQuery {
-          allGitaChapters {
-            nodes {
-              chapterNumber
-              gitaVersesByChapterId {
-                totalCount
-                nodes {
-                  id
-                }
-              }
-            }
-          }
-        }
-      `,
-    })
-    .then((result) => console.log(result));
+
   return (
     <ThemeProvider attribute="class" enableSystem={false}>
-      <ApolloProvider client={client}>
-        {getLayout(<Component {...pageProps} />)}
-      </ApolloProvider>
+      {getLayout(<Component {...pageProps} />)}
     </ThemeProvider>
   );
 }
