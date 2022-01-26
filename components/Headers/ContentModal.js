6c5cc455efd12e9986363bdb3643d60a094613b8
Akshit <akshitarora9211@gmail.com>
2022-01-26 15:18:54 +0530

    add contentModal content

--- components/Headers/ContentModal.js
+++ components/Headers/ContentModal.js
@@ -1,8 +1,11 @@
-import React, { Fragment, useEffect, useRef } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import data from "../../constant/contentModal.json";
+import Link from "next/link";
+import { ArrowNarrowRightIcon } from "@heroicons/react/solid";
 export default function ContentModal({ isOpen, close }) {
-  console.log("Akshit", data);
+  const [selectedChapter, setSelectedChapter] = useState(1);
+
   return (
     <div>
       <Transition appear show={isOpen} as={Fragment}>
@@ -40,8 +43,81 @@ export default function ContentModal({ isOpen, close }) {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white dark:bg-dark-bg shadow-xl rounded-2xl">
-                Content
+              <div className="inline-block w-5/6 max-w-4xl overflow-hidden text-left align-middle transition-all transform bg-white dark:bg-dark-100 shadow-xl rounded-2xl">
+                <div className="flex items-center">
+                  <div className="grid p-4 grid-cols-2">
+                    <div className="">
+                      {" "}
+                      {Array(9)
+                        .fill()
+                        .map((_, index) => (
+                          <div
+                            key={index}
+                            onClick={() => setSelectedChapter(index + 1)}
+                          >
+                            <a className="group p-1 flex items-start rounded-lg hover:bg-my-orange/5 dark:hover:bg-dark-bg hover:cursor-pointer">
+                              <div className=" flex justify-between items-center ml-4">
+                                <p className="text-base font-medium text-gray-500 dark:text-white group-hover:text-my-orange">
+                                  Chapter {index + 1}
+                                </p>
+                                <ArrowNarrowRightIcon
+                                  className="h-5 w-5 ml-4 text-my-orange -translate-x-1 opacity-0 group-hover:opacity-100 group-hover:translate-x-0 transition-transform duration-500"
+                                  aria-hidden="true"
+                                />
+                              </div>
+                            </a>
+                          </div>
+                        ))}
+                    </div>
+                    <div className="">
+                      {Array(9)
+                        .fill()
+                        .map((_, index) => (
+                          <div
+                            key={index}
+                            onClick={() => setSelectedChapter(index + 10)}
+                          >
+                            <a className="group p-1 flex items-start rounded-lg hover:bg-my-orange/5 dark:hover:bg-dark-bg hover:cursor-pointer">
+                              <div className=" flex justify-between items-center ml-4">
+                                <p className="text-base font-medium text-gray-500 dark:text-white group-hover:text-my-orange">
+                                  Chapter {index + 10}
+                                </p>
+                                <ArrowNarrowRightIcon
+                                  className="h-5 w-5 ml-4 text-my-orange -translate-x-1 opacity-0 group-hover:opacity-100 group-hover:translate-x-0 transition-transform duration-500"
+                                  aria-hidden="true"
+                                />
+                              </div>
+                            </a>
+                          </div>
+                        ))}
+                    </div>
+                  </div>
+                  <div className="flex justify-center h-full flex-1 flex-col bg-light-orange  p-6">
+                    <p className="text-my-orange py-2 font-semibold">
+                      Chapter {selectedChapter}
+                    </p>
+                    <hr className="border-my-orange/10" />
+                    <div className={`flex p-3 flex-wrap `}>
+                      {Array(
+                        data.filter(
+                          (chapter) => chapter.chapterNumber === selectedChapter
+                        )[0].gitaVersesByChapterId.totalCount
+                      )
+                        .fill()
+                        .map((_verse, index) => (
+                          <div
+                            onClick={() => {}}
+                            className={`flex justify-center items-center h-10 w-10 p-1.5 m-px text-gray-500 rounded hover:cursor-pointer hover:bg-my-orange hover:text-white 
+                             
+                           
+          `}
+                          >
+                            {index + 1}
+                          </div>
+                        ))}
+                    </div>
+                  </div>
+                </div>
               </div>
             </Transition.Child>
           </div>
