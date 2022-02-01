1625cc848439d14dba8274253d106225bb66d4ec
Akshit <akshitarora9211@gmail.com>
2022-02-01 14:37:18 +0530

    add advanceSetting states to PageLayout

--- layouts/PagesLayout.js
+++ layouts/PagesLayout.js
@@ -1,12 +1,25 @@
-import React from "react";
+import { cloneElement, useState } from "react";
 import Footer from "../components/Footers/Footer";
 import PageHeader from "../components/Headers/PageHeader";
 
 const PagesLayout = ({ children }) => {
+  const [advanceSettings, setAdvanceSettings] = useState({
+    devnagari: true,
+    verseText: true,
+    synonyms: true,
+    translation: true,
+    purport: true,
+  });
+
   return (
-    <div className="dark:bg-dark-bg">
-      <PageHeader />
-      {children}
+    <div className="dark:bg-dark-bg min-h-screen flex flex-col justify-between">
+      <PageHeader
+        advanceSettings={advanceSettings}
+        setAdvanceSettings={setAdvanceSettings}
+      />
+      <div className="flex-1">
+        {cloneElement(children, { advanceSettings })}
+      </div>
       <Footer />
     </div>
   );
