ea22f3bcdfe8c9b660422ae91fc8a16c6262ba77
Akshit <akshitarora9211@gmail.com>
2022-02-02 14:28:24 +0530

    setup store

--- redux/store.js
+++ redux/store.js
@@ -1,17 +1,42 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import { createWrapper } from "next-redux-wrapper";
-import rootReducer from "./reducers/rootReducer";
+import { useMemo } from "react";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from "redux-thunk";
+import reducers from "./reducers/rootReducer";
 
-const middleware = [thunk];
+let store;
 
-const composeEnhancers =
-  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
+function initStore(initialState) {
+  return createStore(
+    reducers,
+    initialState,
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
+  );
+}
 
-const enhancer = composeEnhancers(applyMiddleware(...middleware));
+export const initializeStore = (preloadedState) => {
+  let _store = store ?? initStore(preloadedState);
 
-const makeStore = () => createStore(rootReducer, enhancer);
+  // After navigating to a page with an initial Redux state, merge that state
+  // with the current state in the store, and create a new store
+  if (preloadedState && store) {
+    _store = initStore({
+      ...store.getState(),
+      ...preloadedState,
+    });
+    // Reset the current store
+    store = undefined;
+  }
 
-export const wrapper = createWrapper(makeStore);
+  // For SSG and SSR always create a new store
+  if (typeof window === "undefined") return _store;
+  // Create the store once in the client
+  if (!store) store = _store;
+
+  return _store;
+};
+
+export function useStore(initialState) {
+  const store = useMemo(() => initializeStore(initialState), [initialState]);
+  return store;
+}
