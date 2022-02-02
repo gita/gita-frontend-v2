80272d0375929499978a6e09afe56be9b4f19762
Akshit <akshitarora9211@gmail.com>
2022-02-02 14:29:17 +0530

    add provider and store to `_app.js`

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,20 @@
 import "tailwindcss/tailwind.css";
-import { wrapper } from "../redux/store";
+// import { wrapper } from "../redux/store";
+import { Provider } from "react-redux";
+import { useStore } from "../redux/store";
 import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
+  const store = useStore(pageProps.initialReduxState);
 
   return (
-    <ThemeProvider attribute="class" enableSystem={false}>
-      {getLayout(<Component {...pageProps} />)}
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider attribute="class" enableSystem={false}>
+        {getLayout(<Component {...pageProps} />)}
+      </ThemeProvider>
+    </Provider>
   );
 }
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;
