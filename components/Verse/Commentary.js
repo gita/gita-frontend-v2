143c410d7bdfa862e8860396a6173ce6e3f4bd36
Akshit <akshitarora9211@gmail.com>
2022-02-07 22:37:50 +0530

    comsume data in Commentary compoenent

--- components/Verse/Commentary.js
+++ components/Verse/Commentary.js
@@ -1,43 +1,13 @@
 import React from "react";
 
-export default function Commentary() {
+export default function Commentary({ commentaryData }) {
   return (
     <div>
       <h1 className="font-extrabold text-3xl mt-4 dark:text-gray-50">
         Commentary
       </h1>
       <p className="mt-4 mx-auto text-left dark:text-gray-50">
-        The two armies had gathered on the battlefield of Kurukshetra, well
-        prepared to fight a war that was inevitable. Still, in this verse, King
-        Dhritarashtra asked Sanjay, what his sons and his brother Pandu’s sons
-        were doing on the battlefield? It was apparent that they would fight,
-        then why did he ask such a question?
-      </p>
-      <p className="mt-4 mx-auto text-left dark:text-gray-50">
-        The blind King Dhritarashtra’s fondness for his own sons had clouded his
-        spiritual wisdom and deviated him from the path of virtue. He had
-        usurped the kingdom of Hastinapur from the rightful heirs; the Pandavas,
-        sons of his brother Pandu. Feeling guilty of the injustice he had done
-        towards his nephews, his conscience worried him about the outcome of
-        this battle.
-      </p>
-      <p className="mt-4 mx-auto text-left dark:text-gray-50">
-        The words dharma kṣhetre, the land of dharma (virtuous conduct) used by
-        Dhritarashtra depict the dilemma he was experiencing. Kurukshetra is
-        described as kurukṣhetraṁ deva yajanam in the Shatapath Brahman, the
-        Vedic textbook detailing rituals. It means “Kurukshetra is the
-        sacrificial arena of the celestial gods.” Hence, it was regarded as the
-        sacred land that nourished dharma.
-      </p>
-      <p className="mt-4 mx-auto text-left dark:text-gray-50">
-        Dhritarashtra feared that the holy land might influence the minds of his
-        sons. If it aroused the faculty of discrimination, they might turn away
-        from killing their cousins and negotiate a truce. A peaceful settlement
-        meant that the Pandavas would continue being a hindrance for them. He
-        felt great displeasure at these possibilities, instead preferred that
-        this war transpires. He was uncertain of the consequences of the war,
-        yet desired to determine the fate of his sons. Therefore, he asked
-        Sanjay about the activities of the two armies on the battleground.
+        {commentaryData.description}
       </p>
     </div>
   );
