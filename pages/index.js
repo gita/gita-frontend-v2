65b607bd07b2171f3ac3f93bfee394a67d0d9179
samanyougarg <samanyugarg@gmail.com>
2023-07-09 20:03:27 +0530

    formatting

--- pages/index.js
+++ pages/index.js
@@ -16,18 +16,19 @@ const HASURA_GRAPHQL_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT;
 const HASURA_ADMIN_SECRET = process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET;
 
 if (!HASURA_GRAPHQL_ENDPOINT || !HASURA_ADMIN_SECRET) {
-  throw new Error('Environment variables NEXT_PUBLIC_GRAPHQL_ENDPOINT or NEXT_PUBLIC_HASURA_ADMIN_SECRET are not set');
+  throw new Error(
+    "Environment variables NEXT_PUBLIC_GRAPHQL_ENDPOINT or NEXT_PUBLIC_HASURA_ADMIN_SECRET are not set"
+  );
 }
-
 
 export const getStaticProps = async (props) => {
   // Create an HttpLink that connects to your GraphQL endpoint
   const httpLink = new HttpLink({
     uri: HASURA_GRAPHQL_ENDPOINT,
     headers: {
-      'x-hasura-admin-secret': HASURA_ADMIN_SECRET,
-      'content-type': 'application/json',
-      'hasura-client-name': 'bhagavad-gita-web',
+      "x-hasura-admin-secret": HASURA_ADMIN_SECRET,
+      "content-type": "application/json",
+      "hasura-client-name": "bhagavad-gita-web",
     },
   });
 
@@ -51,8 +52,8 @@ export const getStaticProps = async (props) => {
     `,
   });
   return {
-      props: { chapters: data?.gita_chapters },
-  }
+    props: { chapters: data?.gita_chapters },
+  };
 };
 export default function Home({ chapters }) {
   const dispatch = useDispatch();
