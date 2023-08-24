3abb11d50392a18d497da47ac338275a9badba17
elmeister <elegirmeister@gmail.com>
2023-08-24 21:45:03 +0700

    Report missing translations in getTranslate()

--- src/shared/translate/index.ts
+++ src/shared/translate/index.ts
@@ -1,3 +1,12 @@
-export const getTranslate =
-  (translations: Record<string, string>) => (literal: string) =>
-    translations[literal] || literal;
+export const getTranslate = (translations: Record<string, string>) => {
+  const translationsKeys = Object.keys(translations);
+  return (literal: string) => {
+    const translation = translations[literal];
+    if (translation) {
+      return translation;
+    } else if (translationsKeys.length && typeof window === "undefined") {
+      console.error(`Literal ${literal} not found in translations`);
+    }
+    return literal;
+  };
+};
