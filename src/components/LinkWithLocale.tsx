cba94e03a02afa9ea4a8d562f707652c28a5ddc2
elmeister <elegirmeister@gmail.com>
2023-08-26 22:40:50 +0700

    Support UrlObject in getUpdatedHref()

--- src/components/LinkWithLocale.tsx
+++ src/components/LinkWithLocale.tsx
@@ -1,6 +1,22 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import { UrlObject } from "url";
 
 import { getLocaleFromPath } from "./shared/functions";
+
+const getUpdatedHref = (href: UrlObject | string, locale: Locale) => {
+  const hrefUrl =
+    typeof href === "string"
+      ? new URL(
+          href.startsWith("/") ? `${window.location.origin}${href}` : href,
+        )
+      : href;
+  const addSlash = !hrefUrl.pathname.endsWith("/");
+  hrefUrl.pathname = `${hrefUrl.pathname}${addSlash ? "/" : ""}${locale}`;
+  return hrefUrl.toString();
+};
 
 function LinkWithLocale({
   href,
@@ -8,8 +24,12 @@ function LinkWithLocale({
   ...props
 }: React.PropsWithChildren<React.ComponentProps<typeof Link>>) {
   const locale = getLocaleFromPath();
+
   return (
-    <Link href={locale === "en" ? href : `${href}/${locale}`} {...props}>
+    <Link
+      href={locale === "en" ? href : getUpdatedHref(href, locale)}
+      {...props}
+    >
       {children}
     </Link>
   );
