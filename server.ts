7b6a09f66c21bc823563893c64448d66bc627b69
samanyougarg <samanyugarg@gmail.com>
2023-09-02 01:54:34 +0530

    formatting

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import express from "express";
+import rateLimit from "express-rate-limit";
 import next from "next";
 import path from "path";
-import rateLimit from 'express-rate-limit';
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -10,13 +10,13 @@ const handle = app.getRequestHandler();
 // Enable rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
+  max: 100, // limit each IP to 100 requests per windowMs
 });
 
 app.prepare().then(() => {
   const server = express();
   // Apply rate limiting to the /app route
-  server.use('/app', limiter);
+  server.use("/app", limiter);
 
   // Serve index.html when someone navigates to /app
   server.get("/app", (req, res) => {
