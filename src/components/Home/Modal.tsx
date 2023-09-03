ecb15e8dcf2ef4106f9aa1b1a7c27a118c880901
Gupta-Anubhav12 <anubhavgupta2260@gmail.com>
2023-09-03 20:29:45 +0530

    fix : mutation query and modal on confirmation

--- src/components/Home/Modal.tsx
+++ src/components/Home/Modal.tsx
@@ -1,4 +1,6 @@
 import { Dispatch, SetStateAction } from "react";
+import { Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 
 interface Props {
   modalVisible: boolean;
@@ -7,28 +9,36 @@ interface Props {
 
 function Modal({ modalVisible, setModalVisible }: Props) {
   return (
-    <div className={`relative z-50 ${!modalVisible && "hidden"}`}>
-      <div
-        className="fixed inset-0 z-10 overflow-y-auto"
-        aria-labelledby="modal-title"
-        x-ref="dialog"
-        aria-modal="true"
+    <Transition.Root show={modalVisible} as={Fragment}>
+    <Dialog as="div" className="relative z-10" onClose={setModalVisible}>
+      <Transition.Child
+        as={Fragment}
+        enter="ease-out duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="ease-in duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
       >
-        <div className="flex min-h-screen items-center justify-center px-4 pb-20 pt-4 text-center sm:block sm:p-0">
-          <div
-            className="fixed inset-0 bg-gray-500 opacity-75 transition-opacity dark:bg-dark-bg"
-            aria-hidden="true"
-          ></div>
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+      </Transition.Child>
 
-          {/* <!-- This element is to trick the browser into centering the modal contents. --> */}
-          <span
-            className="hidden sm:inline-block sm:h-screen sm:align-middle"
-            aria-hidden="true"
-          ></span>
-
-          <div className="inline-block overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left align-bottom shadow-xl transition-all duration-200 dark:bg-dark-100 sm:my-8 sm:w-full sm:max-w-sm sm:p-6 sm:align-middle">
-            <div>
-              <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-yellow-100 dark:bg-dark-bg">
+      <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <Dialog.Panel className="relative overflow-hidden rounded-lg bg-white text-left  shadow-xl transition-all sm:w-full sm:max-w-sm">
+              <div>
+              <div className="z-50 inline-block  overflow-hidden rounded-lg bg-white  text-left align-bottom shadow-xl transition-all duration-200 dark:bg-dark-100 sm:mt-8 sm:w-full sm:max-w-sm sm:p-6 sm:align-middle">
+            <div >
+              <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-yellow-100 dark:bg-dark-bg ">
                 <svg
                   className="h-6 w-6 text-yellow-400"
                   x-description="Heroicon name: outline/check"
@@ -71,9 +81,14 @@ function Modal({ modalVisible, setModalVisible }: Props) {
               </button>
             </div>
           </div>
+
+              </div>
+            </Dialog.Panel>
+          </Transition.Child>
         </div>
       </div>
-    </div>
+    </Dialog>
+  </Transition.Root>
   );
 }
 
