14304df00dd78564deb612cef70a9120df863e2b
sarthak-kumar-shailendra <sarthak.shailendra@gmail.com>
2023-09-25 22:34:12 +0530

    clicking outside closes dropdown, scroll behaviour

--- src/app/chapter/[chapterNumber]/(chapter)/[[...locale]]/ChapterPage.tsx
+++ src/app/chapter/[chapterNumber]/(chapter)/[[...locale]]/ChapterPage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import {
   ChevronDownIcon,
   SortAscendingIcon,
@@ -11,6 +11,7 @@ import PageNavigator from "components/Chapter/PageNavigator";
 import VerseList from "components/Chapter/VerseList";
 import VerseNavigator from "components/Chapter/VerseNavigator";
 import { SvgChapterBackground } from "components/svgs";
+import { useClickOutside } from "hooks/useClickOutside";
 import useMyStyles from "hooks/useMyStyles";
 import { classNames } from "shared/functions";
 import { getTranslate } from "shared/translate";
@@ -38,6 +39,8 @@ export default function ChapterPage({
   const [verseId, setVerseId] = useState(0);
   const [isAscSorted, setIsAscSorted] = useState(true);
   const styles = useMyStyles();
+  const toggleRef = useRef<HTMLDivElement>(null);
+  useClickOutside([toggleRef], () => setViewNavigation(false));
 
   const translate = getTranslate(translations, locale);
 
@@ -96,7 +99,7 @@ export default function ChapterPage({
           >
             {verses_count} {translate("Verses")}
           </div>
-          <div className="relative mt-1 flex rounded-md shadow-sm">
+          <div ref={toggleRef} className="relative mt-1 flex rounded-md shadow-sm">
             <div className="relative flex grow items-stretch focus-within:z-10">
               <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 "></div>
               <input
--- src/components/Chapter/VerseNavigator.tsx
+++ src/components/Chapter/VerseNavigator.tsx
@@ -30,7 +30,7 @@ function VerseNavigator({
     <div
       className={classNames(
         fontSize === "large" ? "top-12" : "top-10",
-        `absolute flex w-full flex-wrap rounded border border-gray-200 bg-white p-3 shadow dark:border-dark-100 dark:bg-dark-bg ${
+        `absolute flex w-full max-h-56 overflow-y-scroll flex-wrap rounded border border-gray-200 bg-white p-3 shadow dark:border-dark-100 dark:bg-dark-bg ${
           !viewNavigation && "hidden"
         }`,
       )}
@@ -41,7 +41,7 @@ function VerseNavigator({
           setVerseId(0);
         }}
         className={classNames(
-          `m-px flex min-w-[2.5rem] items-center justify-center rounded p-2 hover:cursor-pointer hover:bg-my-orange hover:text-white ${
+          `m-px flex min-w-[2.35rem] items-center justify-center rounded p-2 hover:cursor-pointer hover:bg-my-orange hover:text-white ${
             !currentVerse && "bg-my-orange text-white"
           }`,
           styles.fontSize.para,
