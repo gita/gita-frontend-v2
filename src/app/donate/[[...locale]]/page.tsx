efc47e1f9eb1cd9c4fe18aa1d750c74c70fe9f46
samanyougarg <samanyugarg@gmail.com>
2024-01-20 23:33:08 +0530

    update donate page

--- src/app/donate/[[...locale]]/page.tsx
+++ src/app/donate/[[...locale]]/page.tsx
@@ -65,10 +65,20 @@ export default async function Donate(props: ParamsWithLocale) {
               Your generous support enables the Ved Vyas Foundation to offer a
               suite of spiritual resources entirely free of charge and devoid of
               distractions. By donating, you help us maintain and expand our
-              offerings, such as the ad-free Bhagavad Gita website and mobile
-              app and Bhagavad Gita AI, ensuring that the essence of Sanatana
-              Dharma reaches the hands and hearts of seekers around the world
-              without any barriers.
+              offerings, such as the ad-free{" "}
+              <a className="text-blue-600 hover:underline" href="/">
+                Bhagavad Gita website
+              </a>
+              ,{" "}
+              <a className="text-blue-600 hover:underline" href="/app">
+                mobile apps
+              </a>{" "}
+              and{" "}
+              <a className="text-blue-600 hover:underline" href="/gitagpt">
+                GitaGPT AI chatbot
+              </a>
+              , ensuring that the essence of Sanatana Dharma reaches the hands
+              and hearts of seekers around the world without any barriers.
             </p>
             <p className="mb-6 text-center text-lg">
               Our dedication is to the digitization and modern presentation of
@@ -86,30 +96,15 @@ export default async function Donate(props: ParamsWithLocale) {
                 height={300}
               />
             </div>
-            <p className="mt-4 text-center">
-              Scan the QR code with any UPI app to make your donation. We are
-              grateful for your partnership in creating a compassionate and
-              harmonious world through the wisdom of Sanatana Dharma.
+            <p className="mt-4 text-center font-bold">
+              Scan the QR code with any UPI app to make your donation.
             </p>
-            <div className="mt-8 text-center">
-              <a
-                href="https://play.google.com/store/apps/details?id=com.gitainitiative.bhagavadgita"
-                className="text-blue-600 hover:underline"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Download our Bhagavad Gita Mobile App
-              </a>
-              <br />
-              <a
-                href="https://bhagavadgita.io/gitagpt"
-                className="text-blue-600 hover:underline"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Try Bhagavad Gita AI
-              </a>
-            </div>
+            <p className="mt-4 text-center">
+              <span className="font-bold">UPI ID:</span>{" "}
+              <span className="font-bold text-my-orange">
+                vedvyasfoundation@icici
+              </span>
+            </p>
           </div>
         </div>
       )}
